0591a236c2ce33f2279c8e86928208d31fe73209
Leon.camkiss <leon.camkiss@gmail.com>
2022-08-09 10:57:01 +0100

    Added B/CSS files

--- src/main/java/com/qa/rpgwc/Beans/CustomBeans.java
+++ src/main/java/com/qa/rpgwc/Beans/CustomBeans.java
@@ -1,5 +1,14 @@
 package com.qa.rpgwc.Beans;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.modelmapper.ModelMapper;
+
+@Configuration
 public class CustomBeans {
 
+	@Bean
+	ModelMapper mapper() {
+		return new ModelMapper();
+	}
 }
---
+++ src/test/java/com/qa/rpgwc/repo/WeaponStatsRepoTest.java
@@ -0,0 +1,5 @@
+package com.qa.rpgwc.repo;
+
+public class WeaponStatsRepoTest {
+
+}
